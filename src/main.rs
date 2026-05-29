19692ea0efbe63811f7956fbe68e80d18ee92c1e
Abdullah Ahmad <ahmada1uk@googlemail.com>
2026-05-29 23:54:01 +0100

    create hello and goodbye function

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,15 @@
 /// Wonderful stuff
 
 fn main() {
+    print_hello();
+    print_goodbye();
+}
+
+
+fn print_hello() {
     println!("Hello, world!");
+}
+
+fn print_goodbye() {
     println!("Goodbye, world!");
 }
