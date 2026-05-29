d3972a43bb5defc26bb962227140e052520e2b28
Abdullah Ahmad <ahmada1uk@googlemail.com>
2026-05-29 23:34:18 +0100

    add even more comments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 /// A "hello world!" program.
+///
+/// This is the best implementation of this program to ever exist
 
 fn main() {
     println!("Hello, world!");
+    println!("Goodbye, world!");
 }
